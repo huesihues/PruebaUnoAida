d83bb016c898b24f83ef0ea152bb9eaff5b7dd74
huesihues <joanhuesoaguilar@gmail.com>
2016-05-29 21:00:31 +0200

    Añadido Player y su funcionamiento

---
+++ VR-Unity-JuegoMiedo/Assets/Scripts/player_movement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_movement : MonoBehaviour 
+{
+	public float speed;
+	public static bool caminando = true;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (caminando) 
+		{
+		Debug.Log("Ahora estoy caminando");
+		}
+
+	
+	}
+	public void OnTriggerEnter ()
+	{
+		caminando = false;
+	}
+	public void OnTriggerExit()
+	{
+		caminando = true;
+	}
+}
