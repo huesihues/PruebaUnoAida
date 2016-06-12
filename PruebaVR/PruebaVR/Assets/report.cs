ef96abc8b531cd8392b107c98204f2012f72d93c
Melontxo88 <mikelnunez88@gmail.com>
2016-06-12 20:55:27 +0200

    borrado

---
+++ PruebaVR/PruebaVR/Assets/report.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class report : MonoBehaviour {
+	public GvrHead head;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("------>>>>");
+		Debug.Log ("------>>>>");
+		Debug.Log ("------>>>>");
+		Debug.Log ("------>>>>");
+		Debug.Log ("------>>>>");
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("rotation " + head.trackRotation);
+	
+	}
+}
